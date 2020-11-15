4e1572113bfcb9a355559b73e4c6a6696551e946
Sanhe Hu <husanhe@gmail.com>
2020-11-14 22:10:44 -0500

    commit

--- application.py
+++ application.py
@@ -6,6 +6,7 @@ from learn_elb.webapp.app import create_app
 # please keep the variable name as it is
 application = create_app()
 
+# THE CODE BELOW IS FOR TESTING ON LOCAL ONLY, please DON't enable it in production!
 # if __name__ == "__main__":
 #     import learn_elb.webapp.config as config
 #     application.debug = True
